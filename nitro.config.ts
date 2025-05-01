a181115056f21f7633d000e891199bf4d0d63868
paulmelero <paul.melero@backmarket.com>
2025-05-01 05:46:35 +0200

    Add CF types, changed / response, Add TS linting

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,8 @@
 //https://nitro.unjs.io/config
+import nitroCloudflareBindings from "nitro-cloudflare-dev";
+
 export default defineNitroConfig({
   srcDir: "server",
   compatibilityDate: "2024-09-19",
+  modules: [nitroCloudflareBindings],
 });
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,13 +1,18 @@
 import { SOURCE } from "~/constants/source";
 
 export default defineEventHandler((event) => {
-  const list: string[] = [];
+  const list: Record<string, number>[] = [];
+
+  const deployUrl = event.context.cloudflare.request.url;
 
   for (const n in SOURCE) {
     const nKey = parseInt(n) as keyof typeof SOURCE;
     for (const m in SOURCE[nKey]) {
       // const mKey = parseInt(m) as keyof (typeof SOURCE)[typeof nKey];
-      list.push(`${n}/${m} - ${Object.values(SOURCE[nKey]).length}`);
+
+      list.push({
+        [`${deployUrl}${n}/${m}`]: Object.values(SOURCE[nKey]).length,
+      });
     }
   }
 
